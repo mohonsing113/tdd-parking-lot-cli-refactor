9ab42841dcd70a9c75dc768469d7d63d47d75c8a
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-27 10:38:54 +0800

    fixed test cases

--- src/test/java/com/oocl/cultivation/test/SmartParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/SmartParkingBoyFacts.java
@@ -17,14 +17,13 @@ class SmartParkingBoyFacts {
     @Test
     void should_select_first_parking_lot() {
 
-        ParkingLot parkingLot1 = new ParkingLot(12);
-        ParkingLot parkingLot2 = new ParkingLot(9);
-        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(parkingLot1, parkingLot2));
-        ParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotCenter);
+        ParkingLot parkingLotWithMoreSpace = new ParkingLot(12);
+        ParkingLot parkingLotWithLessSpace = new ParkingLot(9);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(Arrays.asList(parkingLotWithMoreSpace, parkingLotWithLessSpace));
 
         ParkingLot selectedParkingLot = smartParkingBoy.selectParkingLot().get();
 
-        assertSame(selectedParkingLot, parkingLot1);
+        assertSame(selectedParkingLot, parkingLotWithMoreSpace);
     }
 
     //given 2 parking lots in parking center that capacity are 5 and 9 and assigned to smart parking boy
@@ -33,14 +32,13 @@ class SmartParkingBoyFacts {
     @Test
     void should_select_second_parking_lot() {
 
-        ParkingLot parkingLot1 = new ParkingLot(5);
-        ParkingLot parkingLot2 = new ParkingLot(9);
-        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(parkingLot1, parkingLot2));
-        ParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotCenter);
+        ParkingLot parkingLotWithLessSpace = new ParkingLot(5);
+        ParkingLot parkingLotWithMoreSpace = new ParkingLot(9);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(Arrays.asList(parkingLotWithLessSpace, parkingLotWithMoreSpace));
 
         ParkingLot selectedParkingLot = smartParkingBoy.selectParkingLot().get();
 
-        assertSame(selectedParkingLot, parkingLot2);
+        assertSame(selectedParkingLot, parkingLotWithMoreSpace);
     }
 
     //given 2 parking lots in parking center with both full and assigned to smart parking boy
@@ -48,10 +46,9 @@ class SmartParkingBoyFacts {
     //then car will not parked
     @Test
     void should_not_park_cars_to_parking_lot_if_there_is_not_enough_position_on_both_parking_lot() {
-        ParkingLot parkingLot1 = new ParkingLot(0);
-        ParkingLot parkingLot2 = new ParkingLot(0);
-        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(parkingLot1, parkingLot2));
-        ParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotCenter);
+        ParkingLot parkingLotFull1 = new ParkingLot(0);
+        ParkingLot parkingLoFull2 = new ParkingLot(0);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(Arrays.asList(parkingLotFull1, parkingLoFull2));
 
         smartParkingBoy.park(new Car());
 
--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyFacts.java
@@ -16,31 +16,15 @@ class SuperSmartParkingBoyFacts {
     @Test
     void should_select_second_parking_lot() {
 
-        ParkingLot parkingLot1 = new ParkingLot(5);
-        ParkingLot parkingLot2 = new ParkingLot(2);
-        parkingLot1.park(new ParkingTicket(parkingLot1), new Car());
-
-        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(parkingLot1, parkingLot2));
-        ParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLotCenter);
+        ParkingLot parkingLotWithLessSpaceRate = new ParkingLot();
+        parkingLotWithLessSpaceRate.park(new ParkingTicket(parkingLotWithLessSpaceRate), new Car());
+        ParkingLot parkingLotWithMoreSpaceRate = new ParkingLot();
+        
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(Arrays.asList(parkingLotWithMoreSpaceRate, parkingLotWithLessSpaceRate));
 
         ParkingLot selectedParkingLot = superSmartParkingBoy.selectParkingLot().get();
 
-        assertSame(selectedParkingLot, parkingLot2);
-    }
-
-    //given 2 parking lots in parking center with both full and assigned to SuperSmartParkingBoy
-    //when SuperSmartParkingBoy select parking lot to park car
-    //then car will not parked
-    @Test
-    void should_not_park_cars_to_parking_lot_if_there_is_not_enough_position_on_both_parking_lot() {
-        ParkingLot parkingLot1 = new ParkingLot(0);
-        ParkingLot parkingLot2 = new ParkingLot(0);
-        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(parkingLot1, parkingLot2));
-        ParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLotCenter);
-
-        superSmartParkingBoy.park(new Car());
-
-        assertNull(superSmartParkingBoy.park(new Car()));
+        assertSame(selectedParkingLot, parkingLotWithMoreSpaceRate);
     }
 
 }
